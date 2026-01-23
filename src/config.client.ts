3d9bd77ef0d36282c2fa99f5882832321b2be5ef
Noah Lebovic <lebovic@users.noreply.github.com>
2026-01-22 16:56:16 -0800

    chore: hide debug mode settings by default (#12)

--- src/config.client.ts
+++ src/config.client.ts
@@ -2,12 +2,15 @@ import { z } from "zod"
 
 const clientEnvSchema = z.object({
   NEXT_PUBLIC_DEFAULT_GIT_REPO: z.string().default(""),
+  NEXT_PUBLIC_SHOW_DEBUG_OPTIONS: z.string().default(""),
 })
 
 const env = clientEnvSchema.parse({
   NEXT_PUBLIC_DEFAULT_GIT_REPO: process.env.NEXT_PUBLIC_DEFAULT_GIT_REPO,
+  NEXT_PUBLIC_SHOW_DEBUG_OPTIONS: process.env.NEXT_PUBLIC_SHOW_DEBUG_OPTIONS,
 })
 
 export const clientConfig = {
   defaultGitRepo: env.NEXT_PUBLIC_DEFAULT_GIT_REPO,
+  showDebugOptions: env.NEXT_PUBLIC_SHOW_DEBUG_OPTIONS === "true",
 }
